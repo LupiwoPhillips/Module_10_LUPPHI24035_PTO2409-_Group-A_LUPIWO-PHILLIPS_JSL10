df7f97471f014c6380d4eec3b1052b7595a85b20
Lupiwo Phillips <lupiwonp@gmail.com>
2025-03-23 22:07:33 +0200

    Update escapeVanillaJS.js

--- escapeVanillaJS.js
+++ escapeVanillaJS.js
@@ -35,13 +35,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function findMostRecentBook(books) {
     // 🪲 Bug: Logic error
-    return books.reduce((mostRecent, book) => new Date(book.published) < new Date(mostRecent.published) ? book : mostRecent);
+    return books.reduce((mostRecent, book) => new Date(book.published) > new Date(mostRecent.published) ? book : mostRecent);
 }
 
 function findIntersection(setA, setB) {
     // 🪲 Bug: Incorrect logic
     const intersection = new Set([...setA]);
-    return intersection;
+    return new Set([...SetA].filter(item => setB.has(item)));
 }
 
 async function navigateLabyrinth(directions) {
